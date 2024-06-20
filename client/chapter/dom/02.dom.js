5954d2d8633b08393090a044e173e4578b6b6f9a
chlyun <rkdmf1306@gmail.com>
2024-06-20 09:39:51 +0900

    14

--- client/chapter/dom/02.dom.js
+++ client/chapter/dom/02.dom.js
@@ -26,6 +26,10 @@
 // - querySelectorAll
 // - closest
 
+// default parameter
+
+getNode('.list'); // [li,li]
+
 // 1. id가 visual-section인 section 태그 요소
 const section = document.querySelector('#visual-section');
 
@@ -35,11 +39,11 @@ const list = section.querySelector('.list');
 // 3. list 요소 안에 about 텍스트를 가진 li 태그 요소
 const about = list.querySelector('li:nth-child(2)');
 
-// const aboutLi = [...list.children].find((li)=>{
-//   return li.textContent === 'about';
-// })
+const aboutLi = [...list.children].find((li) => {
+  return li.textContent === 'about';
+});
 
-const aboutLi = [...list.children].find((li) => li.textContent === 'about');
+// const aboutLi = [...list.children].find(li => li.textContent === 'about')
 
 // 4. name 속성이 search-box인 form 태그 요소
 const form = document.querySelector('form[name="search-box"]');
@@ -51,6 +55,7 @@ const children = form.children;
 // 6. form 요소 안에 있는 input 태그 요소
 // const input = form.lastElementChild;
 const input = children[children.length - 1];
+
 /* 문서 대상 확인 */
 // - matches
 // - contains
