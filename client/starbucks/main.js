4689bb31dd17cebe1d2cffaf6ba91677dcc05f3a
chlyun <rkdmf1306@gmail.com>
2024-06-20 10:43:13 +0900

    gsap을이용한 애니메이션

--- client/starbucks/main.js
+++ client/starbucks/main.js
@@ -3,14 +3,23 @@ const depthlist = document.querySelectorAll('.depth');
 const header = document.querySelector('#header');
 const h = (t) => (t.style.height = 0);
 
+// alist.forEach((a) => {
+//   a.addEventListener('mouseenter', () => {
+//     const target = a.lastElementChild;
+
+//     depthlist.forEach(h);
+
+//     target.style.height = '100px';
+//   });
+// });
+
+// header.addEventListener('mouseleave', () => depthlist.forEach(h));
+
+/*global gsap*/
 alist.forEach((a) => {
-  a.addEventListener('mouseenter', () => {
-    const target = a.lastElementChild;
+  const target = a.lastElementChild;
+  const tl = gsap.timeline({ paused: true }).to(target, { height: 100 });
 
-    depthlist.forEach(h);
-
-    target.style.height = '100px';
-  });
+  a.addEventListener('mouseenter', () => tl.play());
+  a.addEventListener('mouseleave', () => tl.reverse());
 });
-
-header.addEventListener('mouseleave', () => depthlist.forEach(h));
