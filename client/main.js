0ecdd9ea6b8c8b85676a71ab6d897bf6aa27a4f1
chlyun <rkdmf1306@gmail.com>
2024-07-03 10:01:31 +0900

    WEBCOMPONET1

--- client/main.js
+++ client/main.js
@@ -1,11 +1,21 @@
-import { getNode, getStorage, setStorage } from './lib/index.js';
+class MyElement extends HTMLElement {
+  constructor() {
+    super();
+  }
 
-const textField = getNode('#textField');
+  connectedCallback() {
+    console.log('탄생함');
+  }
 
-function handleTextField() {
-  const value = this.value;
-
-  setStorage('text', value);
+  disconnectedCallback() {
+    console.log('죽음!');
+  }
 }
 
-textField.addEventListener('input', handleTextField);
+customElements.define('c-element', MyElement);
+
+const elem = document.createElement('c-element');
+
+const app = document.getElementById('app');
+
+app.appendChild(elem);
